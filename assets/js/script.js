15f5a945af7fe6cdc5da27b91052e7aa9e645d67
Jorge Aragão <jorgearagaodesouza@gmail.com>
2024-07-29 21:46:44 -0300

    criação da classe padrão para qualquer personagem

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+// Knight ou Sorcerer - Guerreiro ou Mago
+// LittleMonster ou BigMonster
+
+class Character {
+
+    _life = 1;
+    maxLife = 1;
+    attack = 0;
+    defense = 0;
+
+    constructor(name) {
+        this.name = name;
+    }
+
+    get life() {
+        return this._life;
+    }
+
+    set life(newLife) {
+        this._life = newLife < 0 ? 0 : newLife;
+    }
+}
