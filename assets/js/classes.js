55cfbd29fe1384352611f99393f87bbcff9c0b97
Jorge Aragão <jorgearagaodesouza@gmail.com>
2024-07-30 12:29:58 -0300

    Criação das classes de cenário

--- assets/js/classes.js
+++ assets/js/classes.js
@@ -24,7 +24,7 @@ class Character {
 class Knight extends Character {
     constructor(name) {
         super(name);
-        this.line = 100;
+        this.life = 100;
         this.attack = 10;
         this.defense = 8;
         this.maxLife = this.life;
@@ -58,5 +58,39 @@ class BigMonster extends Character {
         this.attack = 16;
         this.defense = 6;
         this.maxLife = this.life;
+    }
+}
+
+class Stage {
+    constructor(fighter1, fighter2, fighter1El, fighter2El) {
+        this.fighter1 = fighter1;
+        this.fighter2 = fighter2;
+        this.fighter1El = fighter1El;
+        this.fighter2El = fighter2El;
+    }
+
+    start() {
+        this.update();
+
+        this.fighter1El.querySelector('.attackButton').addEventListener('click', () => this.doAttack(this.fighter1, this.fighter2));
+        this.fighter2El.querySelector('.attackButton').addEventListener('click', () => this.doAttack(this.fighter2, this.fighter1));
+    }
+
+    update() {
+        // Fighter 1
+        this.fighter1El.querySelector('.name').innerHTML = `${this.fighter1.name} - ${this.fighter1.life} HP`;
+        let f1Pct = (this.fighter1.life / this.fighter1.maxLife) * 100;
+        this.fighter1El.querySelector('.bar').style.width = `${f1Pct}%`;
+
+        // Fighter 2
+        this.fighter2El.querySelector('.name').innerHTML = `${this.fighter2.name} - ${this.fighter2.life} HP`;
+        let f2Pct = (this.fighter2.life / this.fighter2.maxLife) * 100;
+        this.fighter2El.querySelector('.bar').style.width = `${f2Pct}%`;
+    }
+
+    doAttack(attacking, attacked) {
+        console.log(`${attacking.name} está atacando ${attacked.name}`);
+
+        this.update();
     }
 }
