503deac74682b43d3dc3fdaaf8d784d433799bf7
Jorge Aragão <jorgearagaodesouza@gmail.com>
2024-07-29 21:55:13 -0300

    criação da classe Socerer

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,3 +30,13 @@ class Knight extends Character {
         this.maxLife = this.life;
     }
 }
+
+class Sorcerer extends Character {
+    constructor(name) {
+        super(name);
+        this.life = 80;
+        this.attack = 15;
+        this.defense = 3;
+        this.maxLife = this.life;
+    }
+}
